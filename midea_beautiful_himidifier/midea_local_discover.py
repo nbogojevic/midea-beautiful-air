83ce29ba16f541c1480b2508221dafa561ddb86e
Nenad Bogojevic <nbogojevic@hotmail.com>
2021-12-05 21:29:51 +0100

    Add command line options

--- midea_beautiful_himidifier/midea_local_discover.py
+++ midea_beautiful_himidifier/midea_local_discover.py
@@ -7,6 +7,7 @@ except:
         pass
 import logging
 
+import argparse
 import asyncio
 
 from midea_client_scanner import async_find_devices
@@ -15,21 +16,26 @@ VERSION = '0.1.40'
 
 _LOGGER = logging.getLogger(__name__)
 
-"""
-# 'ip': '10.0.4.210', 'id': 18691697890837, 'port': 6444, 'token': '3B662D0297A0246B9DCBDFF1C8D4B8E6C9309430FDF21B0262D48BBA263BC4098C1035800F2C0395DA43BC9DCA7AFA5AFCFE085149B3727C162E5B192CBB952F', 'key': '45E80C1E977B41C097A9CD3DAB580EFF0235804A62E84701B38CF6A716FFB689', 'ssid': 'net_a1_B5B1'
-# 'ip': '10.0.7.130', 'id': 31885837452002, 'port': 6444, 'token': '9761A7A156C10906F29F78916824DB0388A32A8CE7F6DED9574BDED9B8EBEB18ABB0C952AAFC9D15B3A64249798E3997C7A96542BEE1F0D2FA36CC1DFECA8D78', 'key': 'D1D51863F5C3481A96F1A8EC019AC0DD0905D339F555462390F5130149783197', 'ssid': 'net_a1_7D60'
-"""
 
-
+account = "SOME_ACCOUNT"
+password = "SOME_USER"
 app_key = "3742e9e5842d4ad59c2db887e12449f9"
 use_midea_cloud = False
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Discover Midea dehumidifiers.')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--account')
+    parser.add_argument('--password')
+
+    args = parser.parse_args()
     try:
-        coloredlogs_install(level='DEBUG')
+        coloredlogs_install(level='DEBUG' if args.debug else 'INFO')
     except:
-        logging.basicConfig(level=logging.DEBUG)
-    _LOGGER.info("Allocating new event loop")
+        logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+    account = args.account
+    password = args.password
+    _LOGGER.debug("Allocating new event loop")
     loop = asyncio.new_event_loop()
     try:
         devices = loop.run_until_complete(async_find_devices(app_key=app_key, account=account, password=password))
