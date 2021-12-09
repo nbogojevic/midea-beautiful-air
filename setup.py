7fbb772cc3d74e85efab18bbe3673e1a826917e8
Nenad Bogojevic <nbogojevic@hotmail.com>
2021-12-09 13:17:46 +0100

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
 from setuptools import setup, find_packages
 
-from midea_beautiful_dehumidifier import __version__ 
+from midea_beautiful_dehumidifier import __version__
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='midea-beautiful-dehumidifier',
+    name="midea-beautiful-dehumidifier",
     version=__version__,
-
-    url='https://github.com/nbogojevic/midea-beautiful-dehumidifier',
-    author='Nenad Bogojevic',
-    author_email='nenad.bogojevic@gmail.com',
+    url="https://github.com/nbogojevic/midea-beautiful-dehumidifier",
+    author="Nenad Bogojevic",
+    author_email="nenad.bogojevic@gmail.com",
     license="MIT",
-    description="A library to control Midea Dehumidifiers via the Local area network",
+    description=(
+        "A library to control Midea Dehumidifiers"
+        " via the Local area network"
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
-         "Intended Audience :: Developers",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: MacOS :: MacOS X",
-         "Operating System :: Microsoft :: Windows",
-         "Operating System :: POSIX :: Linux",
-         "Programming Language :: Python :: 3",
-         "Topic :: Home Automation",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
     ],
     install_requires=[
-        'cryptography',
-        'requests',
-        'ifaddr'
+        "cryptography>=3.4.8",
+        "requests>=2.26.0",
+        "ifaddr>=0.1.7",
     ],
 )
