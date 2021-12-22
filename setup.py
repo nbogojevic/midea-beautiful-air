c92a7e2ec9ef3ca99b89eaf549d284ad6f2b6e6a
Nenad Bogojevic <nbogojevic@hotmail.com>
2021-12-22 13:21:16 +0100

    Add entrypoint

--- setup.py
+++ setup.py
@@ -3,20 +3,12 @@ import os
 from setuptools import find_packages, setup
 
 
-def read(rel_path: str) -> str:
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with open(os.path.join(here, rel_path)) as fp:
-        return fp.read()
-
-
 def get_version(relative_path: str) -> str:
     package_root = os.path.abspath(os.path.dirname(__file__))
     version = {}
     with open(os.path.join(package_root, relative_path)) as version_fp:
         exec(version_fp.read(), version)
-        return version['__version__']
+        return version["__version__"]
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -30,8 +22,7 @@ setup(
     author_email="nenad.bogojevic@gmail.com",
     license="MIT",
     description=(
-        "A library to control Midea dehumidifiers"
-        " via the Local area network"
+        "A library to control Midea dehumidifiers" " via the Local area network"
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -45,6 +36,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Home Automation",
     ],
+    entry_points="""
+        [console_scripts]
+        midea-beautiful-dehumidifier-cli=midea_beautiful_dehumidifier.cli:cli
+    """,
     install_requires=[
         "cryptography>=3.4",
         "requests>=2.25.1",
