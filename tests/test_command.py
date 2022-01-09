846ad8c0867b7b2fd8b561a7b6445ad4328d1fd5
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-09 01:16:25 +0100

    Simplify setting attributes

--- tests/test_command.py
+++ tests/test_command.py
@@ -2,6 +2,7 @@ from binascii import unhexlify
 from typing import Final
 
 import pytest
+from midea_beautiful.appliance import AirConditionerAppliance, DehumidifierAppliance
 
 from midea_beautiful.command import (
     AirConditionerResponse,
@@ -11,7 +12,6 @@ from midea_beautiful.command import (
     DeviceCapabilitiesCommandMore,
     midea_command_reset_sequence,
 )
-from midea_beautiful.lan import LanDevice
 from midea_beautiful.midea import (
     APPLIANCE_TYPE_AIRCON,
     APPLIANCE_TYPE_DEHUMIDIFIER,
@@ -27,12 +27,12 @@ def setup_function(fun):
 
 @pytest.fixture(name="dehumidifier")
 def dehumidifier():
-    return LanDevice(id="12345", appliance_type=APPLIANCE_TYPE_DEHUMIDIFIER)
+    return DehumidifierAppliance(id="12345", appliance_type=APPLIANCE_TYPE_DEHUMIDIFIER)
 
 
 @pytest.fixture(name="aircon")
 def aircon():
-    return LanDevice(id="12345", appliance_type=APPLIANCE_TYPE_AIRCON)
+    return AirConditionerAppliance(id="12345", appliance_type=APPLIANCE_TYPE_AIRCON)
 
 
 def test_device_capabilities_command() -> None:
@@ -45,43 +45,43 @@ def test_device_capabilities_command_more() -> None:
     assert "aa0ea100000000000303b501011381" == dc.finalize().hex()
 
 
-def test_dehumidifier_status(dehumidifier: LanDevice) -> None:
-    cmd = dehumidifier.state.refresh_command().finalize()
+def test_dehumidifier_status(dehumidifier: DehumidifierAppliance) -> None:
+    cmd = dehumidifier.refresh_command().finalize()
     assert (
         "aa20a100000000000003418100ff03ff000000000000000000000000000001294f"
         == cmd.hex()
     )
 
 
-def test_dehumidifier_set(dehumidifier: LanDevice) -> None:
-    cmd = dehumidifier.state.apply_command().finalize()
+def test_dehumidifier_set(dehumidifier: DehumidifierAppliance) -> None:
+    cmd = dehumidifier.apply_command().finalize()
     assert (
         "aa20a100000000000302480000280000003200000000000000000000000001395e"
         == cmd.hex()
     )
 
 
-def test_dehumidifier_set_fan_speed(dehumidifier: LanDevice) -> None:
-    setattr(dehumidifier.state, "fan_speed", 60)
-    cmd = dehumidifier.state.apply_command().finalize()
+def test_dehumidifier_set_fan_speed(dehumidifier: DehumidifierAppliance) -> None:
+    dehumidifier.fan_speed = 60
+    cmd = dehumidifier.apply_command().finalize()
     assert (
         "aa20a1000000000003024800003c0000003200000000000000000000000001dea5"
         == cmd.hex()
     )
 
 
-def test_dehumidifier_set_mode(dehumidifier: LanDevice) -> None:
-    setattr(dehumidifier.state, "mode", 3)
-    cmd = dehumidifier.state.apply_command().finalize()
+def test_dehumidifier_set_mode(dehumidifier: DehumidifierAppliance) -> None:
+    dehumidifier.mode = 3
+    cmd = dehumidifier.apply_command().finalize()
     assert (
         "aa20a1000000000003024800032800000032000000000000000000000000014b49"
         == cmd.hex()
     )
 
 
-def test_dehumidifier_set_target_humidity(dehumidifier: LanDevice) -> None:
-    setattr(dehumidifier.state, "target_humidity", 45)
-    cmd = dehumidifier.state.apply_command().finalize()
+def test_dehumidifier_set_target_humidity(dehumidifier: DehumidifierAppliance) -> None:
+    dehumidifier.target_humidity = 45
+    cmd = dehumidifier.apply_command().finalize()
     assert (
         "aa20a100000000000302480000280000002d000000000000000000000000017626"
         == cmd.hex()
@@ -109,63 +109,63 @@ def test_dehumidifier_set_various() -> None:
     )
 
 
-def test_ac_status(aircon: LanDevice) -> None:
-    cmd = aircon.state.refresh_command().finalize()
+def test_ac_status(aircon: AirConditionerAppliance) -> None:
+    cmd = aircon.refresh_command().finalize()
     assert (
         "aa20ac00000000000003418100ff03ff00020000000000000000000000000171fa"
         == cmd.hex()
     )
 
 
-def test_ac_status_in_sequence(aircon: LanDevice) -> None:
+def test_ac_status_in_sequence(aircon: AirConditionerAppliance) -> None:
     midea_command_reset_sequence(2)
-    cmd = aircon.state.refresh_command().finalize()
+    cmd = aircon.refresh_command().finalize()
     assert (
         "aa20ac00000000000003418100ff03ff000200000000000000000000000003cd9c"
         == cmd.hex()
     )
 
 
-def test_aircon_set_fan(aircon: LanDevice) -> None:
-    setattr(aircon.state, "beep_prompt", True)
-    setattr(aircon.state, "fan_speed", 48)
-    cmd = aircon.state.apply_command().finalize()
+def test_aircon_set_fan(aircon: AirConditionerAppliance) -> None:
+    aircon.beep_prompt = True
+    aircon.fan_speed = 48
+    cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac0000000000000240400030000000000000100000000000000000000100000078f6"
         == cmd.hex()
     )
 
 
-def test_aircon_set_mode(aircon: LanDevice) -> None:
-    setattr(aircon.state, "beep_prompt", True)
-    setattr(aircon.state, "fan_speed", 48)
-    setattr(aircon.state, "mode", 2)
-    cmd = aircon.state.apply_command().finalize()
+def test_aircon_set_mode(aircon: AirConditionerAppliance) -> None:
+    aircon.beep_prompt = True
+    aircon.fan_speed = 48
+    aircon.mode = 2
+    cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac00000000000002404040300000000000001000000000000000000001000000ce60"
         == cmd.hex()
     )
 
 
-def test_aircon_set_turbo(aircon: LanDevice) -> None:
-    setattr(aircon.state, "beep_prompt", True)
-    setattr(aircon.state, "turbo", True)
-    setattr(aircon.state, "fan_speed", 40)
+def test_aircon_set_turbo(aircon: AirConditionerAppliance) -> None:
+    aircon.beep_prompt = True
+    aircon.turbo = True
+    aircon.fan_speed = 40
 
-    cmd = aircon.state.apply_command().finalize()
+    cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac000000000000024040002800000000000012000000000000000000010000000173"
         == cmd.hex()
     )
 
 
-def test_aircon_set_temperature(aircon: LanDevice) -> None:
-    setattr(aircon.state, "beep_prompt", True)
-    setattr(aircon.state, "turbo", True)
-    setattr(aircon.state, "fan_speed", 45)
-    setattr(aircon.state, "target_temperature", 20.5)
+def test_aircon_set_temperature(aircon: AirConditionerAppliance) -> None:
+    aircon.beep_prompt = True
+    aircon.turbo = True
+    aircon.fan_speed = 45
+    aircon.target_temperature = 20.5
 
-    cmd = aircon.state.apply_command().finalize()
+    cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac000000000000024040142d0000000000001200000000000000000001000000a7b4"
         == cmd.hex()
