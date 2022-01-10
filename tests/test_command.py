0be2c6eb12458f7eabcab36e6e2b6c179a4a9af3
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-10 11:08:52 +0100

    Add more tests

--- tests/test_command.py
+++ tests/test_command.py
@@ -6,6 +6,7 @@ from midea_beautiful.appliance import AirConditionerAppliance, DehumidifierAppli
 
 from midea_beautiful.command import (
     AirConditionerResponse,
+    AirConditionerSetCommand,
     DehumidifierResponse,
     DehumidifierSetCommand,
     DeviceCapabilitiesCommand,
@@ -126,6 +127,26 @@ def test_ac_status_in_sequence(aircon: AirConditionerAppliance) -> None:
     )
 
 
+def test_aircon_defaults() -> None:
+    cmd = AirConditionerSetCommand()
+    assert not cmd.beep_prompt
+    assert not cmd.comfort_sleep
+    assert not cmd.dryer
+    assert not cmd.eco_mode
+    assert not cmd.fahrenheit
+    assert not cmd.horizontal_swing
+    assert not cmd.purifier
+    assert not cmd.running
+    assert not cmd.screen
+    assert not cmd.turbo
+    assert not cmd.turbo_fan
+    assert not cmd.vertical_swing
+    assert cmd.fan_speed == 0
+    assert cmd.mode == 0
+    assert cmd.temperature == 16
+    assert cmd.temperature_decimal == 0
+
+
 def test_aircon_set_fan(aircon: AirConditionerAppliance) -> None:
     aircon.beep_prompt = True
     aircon.fan_speed = 48
@@ -151,7 +172,6 @@ def test_aircon_set_turbo(aircon: AirConditionerAppliance) -> None:
     aircon.beep_prompt = True
     aircon.turbo = True
     aircon.fan_speed = 40
-
     cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac000000000000024040002800000000000012000000000000000000010000000173"
@@ -164,7 +184,6 @@ def test_aircon_set_temperature(aircon: AirConditionerAppliance) -> None:
     aircon.turbo = True
     aircon.fan_speed = 45
     aircon.target_temperature = 20.5
-
     cmd = aircon.apply_command().finalize()
     assert (
         "aa23ac000000000000024040142d0000000000001200000000000000000001000000a7b4"
@@ -183,6 +202,26 @@ def test_dehumidifier_response():
     assert response.off_timer["hour"] == 31
     assert not response.on_timer["status"]
     assert response.on_timer["hour"] == 0
+    assert "'fault': False" in str(response)
+    assert "'current_humidity': 63" in str(response)
+
+
+def test_dehumidifier_response_big_values():
+    response = DehumidifierResponse(
+        unhexlify("c80101287f7f007c00000000000000003fa200000000024238")
+    )
+    assert response.target_humidity == 100
+    assert response.indoor_temperature == 50
+    assert "'fault': False" in str(response)
+
+
+def test_dehumidifier_response_short():
+    response = DehumidifierResponse(unhexlify("c80101287f7f003c00000000000000003f5238"))
+    assert response.current_humidity == 63
+    assert not response.tank_full
+    assert response.target_humidity == 60
+    assert response.light_class is None
+    assert "'light_class': None" in str(response)
 
 
 def test_ac_response():
@@ -195,6 +234,22 @@ def test_ac_response():
     response = AirConditionerResponse(
         unhexlify("c80101287f7f003c00000040200000003f5000000000024238")
     )
-    print(response)
     assert response.outdoor_temperature == -9
     assert response.indoor_temperature == 7
+
+    response = AirConditionerResponse(
+        unhexlify("c80101287f7f003c00000041210000003f5000000000024238")
+    )
+    assert response.outdoor_temperature == -8.5
+    assert response.indoor_temperature == 7.5
+    response = AirConditionerResponse(
+        unhexlify("c80101287f7f003c00000042240000243f5000000000024238")
+    )
+    assert response.outdoor_temperature == -7.2
+    assert response.indoor_temperature == 8.4
+    response = AirConditionerResponse(
+        unhexlify("c80101287f7f003c00000022520000373f5000000000024238")
+    )
+    assert response.outdoor_temperature == 16.3
+    assert response.indoor_temperature == -8.7
+    assert "'indoor_temperature': -8.7" in str(response)
