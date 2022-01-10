5f67b022ba5aeb1a273d3397c1cb2859172238e9
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-11 00:39:14 +0100

    Add docstrings

--- midea_beautiful/exceptions.py
+++ midea_beautiful/exceptions.py
@@ -1,8 +1,11 @@
+"""Exceptions raised by library"""
 from __future__ import annotations
 
 
 class MideaError(Exception):
+    """Base exception for all library specific exceptions"""
     def __init__(self, message: str) -> None:
+        super().__init__(message)
         self.message = message
 
     def __str__(self) -> str:
@@ -10,23 +13,26 @@ class MideaError(Exception):
 
 
 class MideaNetworkError(MideaError):
-    pass
+    """Network problem"""
 
 
 class ProtocolError(MideaError):
-    pass
+    """Problem while parsing payloads"""
 
 
 class UnsupportedError(MideaError):
-    pass
+    """Unsupported appliance or protocol version"""
 
 
 class AuthenticationError(MideaError):
-    pass
+    """Problem while authenticating"""
 
 
 class CloudError(MideaError):
+    """Problem while working with cloud API"""
+
     def __init__(self, error_code: int, message: str) -> None:
+        super().__init__(message)
         self.error_code = error_code
         self.message = message
 
@@ -35,11 +41,14 @@ class CloudError(MideaError):
 
 
 class CloudRequestError(MideaError):
-    pass
+    """Problem while communicating with cloud API"""
 
 
 class RetryLaterError(MideaError):
+    """Too many calls to cloud API. Try later."""
+
     def __init__(self, error_code, message: str) -> None:
+        super().__init__(message)
         self.error_code = error_code
         self.message = message
 
@@ -48,7 +57,10 @@ class RetryLaterError(MideaError):
 
 
 class CloudAuthenticationError(MideaError):
+    """Problem while authenticating with cloud API"""
+
     def __init__(self, error_code, message: str) -> None:
+        super().__init__(message)
         self.error_code = error_code
         self.message = message
 
